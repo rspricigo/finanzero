723288769415201030afdeebe124fd80fb823851
Reginaldo Spricigo <reginaldospricigo@gmail.com>
2025-11-08 16:30:42 -0300

    feat: Configure import alias and initialize Shadcn/ui with first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-3xl font-bold underline bg-sky-500 p-4">FinanZero</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="min-h-screen bg-zinc-900 text-white p-8">
+      <h1 className="text-4xl font-bold mb-4">FinanZero</h1>
+      <p className="text-lg">Em breve, seu gestor financeiro estará aqui!</p>
+    </div>
   );
 }
 
